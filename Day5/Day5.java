962639e7d9ffaa263fbb2d9a87be8c7d43dc5287
KevinCodd <61719515+KevinCodd@users.noreply.github.com>
2022-12-05 22:20:13 -0500

    Day 5

---
+++ Day5/Day5.java
@@ -0,0 +1,56 @@
+import java.util.*;
+public class Day5 {
+
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        ArrayList<String> input = new ArrayList<String>();
+
+        while(line.length()>0){
+            input.add(line);
+            line = sc.nextLine();
+        }
+
+        String [] stack_nums = input.get(input.size()-1).split(" ");
+
+        int num_stacks = Integer.parseInt(stack_nums[stack_nums.length-1]);
+
+        ArrayList<LinkedList<Character>> stacks = new ArrayList<LinkedList<Character>>();
+        for(int i=0; i<num_stacks; i++){
+            stacks.add(new LinkedList<Character>());
+        }
+
+        for(int i = input.size()-2;i>-1;i--){
+            for(int j = 1; j<input.get(i).length(); j+=4){
+                char c = input.get(i).charAt(j);
+
+                if (Character.isAlphabetic(c)) {
+                    stacks.get((j-1)/4).add(c);
+                }
+            }
+        }
+
+        while(sc.hasNextLine()){
+            // String ret = "";
+            // for(LinkedList<Character> stack: stacks){
+            //     ret+=Character.toString(stack.peekLast());
+            // }
+            // System.out.println(ret);
+            String [] linesplit = sc.nextLine().split(" ");
+            int moves = Integer.parseInt(linesplit[1]);
+            int from = Integer.parseInt(linesplit[3])-1;
+            int to = Integer.parseInt(linesplit[5])-1;
+            for(int i = 0; i< moves; i++){
+                char c = stacks.get(from).removeLast();
+                stacks.get(to).add(c);
+            }
+        }
+
+        String ret = "";
+        for(LinkedList<Character> stack: stacks){
+            ret+=Character.toString(stack.peekLast());
+        }
+        System.out.println(ret);
+
+    }
+}
