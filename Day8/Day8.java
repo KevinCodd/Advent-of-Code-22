0765472e4bbfe8aaff60912a033931760a8f4818
KevinCodd <61719515+KevinCodd@users.noreply.github.com>
2022-12-08 17:37:10 -0500

    less space and potentially faster

--- Day8/Day8.java
+++ Day8/Day8.java
@@ -17,73 +17,52 @@ public class Day8 {
                 grid[i][j] = Integer.parseInt(cur.substring(j,j+1));
             }
         }
-        boolean [][] saw = new boolean [grid.length][grid[0].length];
-
         int count = 0;
+        for(int i = 0; i<grid.length; i++){
+            for(int j = 0; j<grid[0].length; j++){
+                if(visScan(grid,i,j)) count++;
+            }
+        }
 
-        for(int i = 0; i<grid[0].length; i++){
-            saw[0][i] = true;
-            saw[grid.length-1][i]=true;
-            count+=2;
-
-        }
-        for(int i = 1; i<grid.length-1; i++){
-            saw[i][0]=true;
-            saw[i][grid[i].length-1] = true;
-            count+=2;
-        }
-        //left -> right
-        for(int i= 1; i<grid.length-1; i++){
-            int highest = grid[i][0];
-            for(int j=1; j<grid[i].length-1; j++) {
-                if(grid[i][j]>highest){
-                    highest = grid[i][j];
-                    if(!saw[i][j]){
-                        saw[i][j]=true;
-                        count++;
-                    }
-                }
-            }
-        }
-        //right --> left
-        for(int i= 1; i<grid.length-1; i++){
-            int highest = grid[i][grid[i].length-1];
-            for(int j=grid[i].length-2; j>0; j--) {
-                if(grid[i][j]>highest){
-                    highest = grid[i][j];
-                    if(!saw[i][j]){
-                        saw[i][j]=true;
-                        count++;
-                    }
-                }
-            }
-        }
-        //up -> down
-        for(int j= 1; j<grid[0].length-1; j++){
-            int highest = grid[0][j];
-            for(int i=1; i<grid.length-1; i++) {
-                if(grid[i][j]>highest){
-                    highest = grid[i][j];
-                    if(!saw[i][j]){
-                        saw[i][j]=true;
-                        count++;
-                    }
-                }
-            }
-        }
-        //down -> up
-        for(int j= 1; j<grid[0].length-1; j++){
-            int highest = grid[grid.length-1][j];
-            for(int i=grid.length-2; i>0; i--) {
-                if(grid[i][j]>highest){
-                    highest = grid[i][j];
-                    if(!saw[i][j]){
-                        saw[i][j]=true;
-                        count++;
-                    }
-                }
-            }
-        }
         System.out.println(count);
+    }
+
+    public static boolean visScan(int [][] grid, int r, int c){
+
+        if(r==0||r==grid.length-1||c==0||c==grid[0].length-1) return true;
+
+        //north
+        for(int i = r-1; i>-1; i--){
+            if(grid[i][c]<grid[r][c]) {
+                if(i==0) return true;
+                else continue;
+            }
+            else break;
+        }
+        //south
+        for(int i = r+1; i<grid.length; i++){
+            if(grid[i][c]<grid[r][c]) {
+                if(i==grid.length-1) return true;
+                else continue;
+            }
+            else break;
+        }
+        //east
+        for(int i = c+1; i<grid[0].length; i++){
+            if(grid[r][i]<grid[r][c]) {
+                if(i==grid[0].length-1) return true;
+                else continue;
+            }
+            else break;
+        }
+        //west
+        for(int i = c-1; i>-1; i--){
+            if(grid[r][i]<grid[r][c]) {
+                if(i==0) return true;
+                else continue;
+            }
+            else break;
+        }
+        return false;
     }
 }
